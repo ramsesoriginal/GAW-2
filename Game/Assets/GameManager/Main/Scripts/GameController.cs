4e505cdb5dae724d1c2f2bc14d3ba5a23c6a85a2
Stefan Insam <ramsesoriginal@gmail.com>
2014-11-06 01:29:39 +0100

    Created Unity Project

---
+++ Game/Assets/GameManager/Main/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject gameModel;
+
+	private GameObject Data;
+	public GameModel data {
+		get {
+			return Data.GetComponent<GameModel>();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Data = GameObject.FindGameObjectWithTag ("GameModel");
+		if (Data == null) {
+			Data = (GameObject) Instantiate(gameModel,transform.position, transform.rotation);
+			Data.name = gameModel.name;
+			data.InitialInit();
+		}
+		data.CurrentSceneInit ();
+	}
+
+}
---
+++ Game/Assets/GameManager/Main/Scripts/GameModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModel : MonoBehaviour {
+
+	private static GameModel current;
+	public static GameModel Current {
+		get {
+			return GameModel.current;
+		}
+	}
+
+	public void InitialInit() {
+		DontDestroyOnLoad (gameObject);
+		current = this;
+	}
+
+	public void CurrentSceneInit() {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

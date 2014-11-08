75ffbd63eb104778666a4a0600d4c00bc854da88
Stefan Insam <ramsesoriginal@gmail.com>
2014-11-08 15:36:58 +0100

    Movement Animations

---
+++ Game/Assets/Gameplay/Scripts/Locomotion.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Locomotion : MonoBehaviour {
+
+	public float Speed;
+	public bool Kneel;
+	public bool Jump;
+
+	private Animator ani;
+	private Animator Animation {
+		get {
+			if (ani==null) 
+				ani = GetComponent<Animator>();
+			return ani;
+		}
+	}
+
+	private int SpeedId {
+		get {
+			return Animator.StringToHash("Speed");
+		}
+	}
+	
+	private int KneelId {
+		get {
+			return Animator.StringToHash("Kneel");
+		}
+	}
+	
+	private int JumplId {
+		get {
+			return Animator.StringToHash("Jump");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Speed = Input.GetAxis ("Horizontal") * 2;
+		Kneel = Input.GetButton ("Kneel");
+		Jump = Input.GetButton ("Jump");
+		Animation.SetBool (KneelId,Kneel);
+		Animation.SetBool (JumplId,Jump);
+		Animation.SetFloat (SpeedId,Speed);
+	}
+}
